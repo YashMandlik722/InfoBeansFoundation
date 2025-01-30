f885fa5391875b802c2c045f0dd5d52a39d9382e
YashMandlik722 <mandlikyash722@gmail.com>
2025-01-31 00:57:51 +0530

    Mail sending feature | Added bulk result from Excel to Db

--- backend/router/resultRouter.js
+++ backend/router/resultRouter.js
@@ -1,9 +1,12 @@
-import express from "express"
-import { myresult } from "../controller/resultController.js"
+import express from "express";
+import multer from "multer";
+import { bulkResult, myresult } from "../controller/resultController.js";
 
 
-const router  =  express.Router()
+const router  =  express.Router();
+const upload = multer();
 
-router.get("/myresult", myresult)
+router.get("/myresult", myresult);
+router.post("/bulkResult", upload.single('file'), bulkResult);
 
-export default router
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,37 @@
+//Dependencies
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//Routers
 import userRouter from "./router/userRouter.js";
 import registerRouter from "./router/courseRegistrationRouter.js";
 import resultRouter from "./router/resultRouter.js";
-import staffRouter from "./router/staffRouter.js"
-import "./DBConfig/connection.js"
+import staffRouter from "./router/staffRouter.js";
+
+//Db Connection
+import "./DBConfig/connection.js";
+
+
 const app = express();
 
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use("/user",userRouter)
-app.use("/register",registerRouter)
-app.use("/result",resultRouter)
-app.use("/staff",staffRouter)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
 
-app.get("/",(req,res)=>{
-res.end("running")
-})
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/user",userRouter);
+app.use("/register",registerRouter);
+app.use("/result",resultRouter);
+app.use("/staff",staffRouter);
 
 app.listen(3001,()=>{
-    console.log("running on localhost:3001")
+    console.log("running on localhost:3001");
 })
