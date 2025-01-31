0fde4692f0423abbb86a2237263908e628a5b1b7
hemant <aryangwale8827@gmail.com>
2025-01-31 15:30:25 +0530

    multer final update

--- backend/helper/multerConfig.js
+++ backend/helper/multerConfig.js
@@ -2,11 +2,44 @@ import multer from "multer";
 
 const registrationDoc = multer.diskStorage({
     destination: (req,file,callback)=>{
-        callback(null, "public/")
+        if(file.fieldname == "studentAadhar"  || file.fieldname == "fatherAadhar"){
+            callback(null, "public/userDoc/aadhar")
+        }
+        else if(file.fieldname == "marksheet12"  || file.fieldname == "marksheetLatest"){
+            callback(null, "public/userDoc/marksheet")
+        }
+        else if(file.fieldname == "pic" ){
+            callback(null, "public/userDoc/photo")
+        }
+        else if(file.fieldname == "incomeCertificate" ){
+            callback(null, "public/userDoc/incomeCertificate")
+        }
+        else if(file.fieldname == "samagraId" ){
+            callback(null, "public/userDoc/samagra")
+        }
     },
     filename: (req,file,callback)=>{
-        const uniqueName = Date.now() + file.originalname
+        const uniqueName = Date.now() + file.originalname;
+        callback(null, uniqueName)
     }
 })
 
-export const uploadRegistration = multer({storage: registrationDoc});
+export const uploadRegistrationDoc = multer({storage: registrationDoc});
+
+const staffDoc = multer.diskStorage({
+    destination: (req,file,callback)=>{
+        if(file.fieldname == "aadhar"){
+            callback(null, "public/staffDoc/aadhar")
+        }
+        else if(file.fieldname == "pic" ){
+            callback(null, "public/staffDoc/photo")
+        }
+        
+    },
+    filename: (req,file,callback)=>{
+        const uniqueName = Date.now() + file.originalname;
+        callback(null, uniqueName)
+    }
+})
+
+
